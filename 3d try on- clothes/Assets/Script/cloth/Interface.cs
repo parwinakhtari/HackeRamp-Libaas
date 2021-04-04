c490d3991ff91d43acfb2fb8b4660f0dcf4eee56
Parwin Akhtari <68842515+parwinakhtari@users.noreply.github.com>
2021-04-04 20:47:32 +0530

    Add files via upload

---
+++ 3d try on- clothes/Assets/Script/cloth/Interface.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Interface : MonoBehaviour, IPointerClickHandler
+{
+	private GameObject humanObject; //Reference to the main model (Human)
+
+    private ChangeClothes changeClothesScript; //Reference to the change Item script
+    private AttachClothing attachScript; //Reference 
+    private Text textChild;  //Their text lol can prob delete this l8r
+	public string modelSize; // s,m,l
+
+	//animation stuff 
+	//https://www.youtube.com/watch?v=4Rc3dlERSWg
+
+	public Animator anim; //animation controller for human
+	public int activePos; // 0, 1, 2
+
+    private ModelSave modelSave;
+    private GameObject block;
+
+
+    /// animation
+    /// 
+    /// 	//rotation
+
+
+    private void Start()
+    {
+        modelSave = FindObjectOfType<ModelSave>();
+		modelSize = "s";
+		activePos = 0; // nothing
+		//anim.SetInteger("Pose", activePos); 
+
+		if(GameObject.FindGameObjectWithTag("Unit")){ 
+			anim = GameObject.FindGameObjectWithTag("Unit").GetComponent<Animator>();
+		
+       		humanObject = GameObject.FindGameObjectWithTag("Unit").gameObject; //finding the human
+
+			//Reference to the changeGear script to use functions
+        	changeClothesScript = humanObject.GetComponent<ChangeClothes>();
+
+			//Reference to the equipment script to use functions
+        	attachScript = humanObject.GetComponent<AttachClothing>();
+            //AddOrRemoveClothes ("naked", "Dress", "underwear01", 0);
+        }
+    }
+
+	private void Update (){
+		if (modelSave) {
+			setClothingSizes (modelSave.size);
+
+		}
+	}
+
+	public void setClothingSizes(string inputSize){
+		modelSize = inputSize;
+        Debug.Log("recommended size: " + inputSize);
+	}
+
+
+    public void OnPointerClick(PointerEventData eventData)
+
+    {
+		string size = modelSize; //update this luk
+
+        if (gameObject.name == "Dress1") {
+			Debug.Log ("Dress 1");
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			AddOrRemoveClothes ("naked", "Dress", "dress01_" + modelSize, 0);
+
+		} else if (gameObject.name == "Dress2") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 2");
+			AddOrRemoveClothes ("naked", "Dress", "dress02_"+ modelSize, 0);
+
+		} else if (gameObject.name == "Dress3") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 3");
+			AddOrRemoveClothes ("naked", "Dress", "dress03_" + modelSize, 0);
+
+		} else if (gameObject.name == "Dress4") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 4");
+			AddOrRemoveClothes ("naked", "Dress", "dress04_" + modelSize, 0);
+
+		} else if (gameObject.name == "Dress5") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 5");
+			AddOrRemoveClothes ("naked", "Dress", "dress05_" + modelSize, 0);
+		}
+		else if (gameObject.name == "Dress6") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 6");
+			AddOrRemoveClothes ("naked", "Dress", "dress06_" + modelSize, 0);
+		}
+		else if (gameObject.name == "Dress7") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 7");
+			AddOrRemoveClothes ("naked", "Dress", "dress07_" + modelSize, 0);
+		}
+		else if (gameObject.name == "Dress8") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 8");
+			AddOrRemoveClothes ("naked", "Dress", "dress08_" + modelSize, 0);
+
+		}
+		else if (gameObject.name == "Dress9") {
+			changeClothesScript.RemoveClothingItem ("Dress", "abc");
+			Debug.Log ("Dress 8");
+			AddOrRemoveClothes ("naked", "Dress", "dress09_" + modelSize, 0);
+
+		}
+			
+		//https://docs.unity3d.com/ScriptReference/Animation-clip.html
+		//ANIMATION https://unity3d.com/learn/tutorials/topics/animation/animator-scripting
+		else if (gameObject.name == "PoseButton") {
+			anim = GameObject.FindGameObjectWithTag("Unit").GetComponent<Animator>();
+
+			Debug.Log ("Pose 1 Animation");
+
+			activePos = 1;
+			anim.SetInteger("Pose", activePos); 
+			//anim.
+			//anim.SetInteger("Pose", 0); 
+
+			//FIX BUG
+			//https://answers.unity.com/questions/612051/change-boolean-after-animation-is-done.html
+
+		}
+		//ANIMATION
+		else if (gameObject.name == "PoseButton2") {
+			anim = GameObject.FindGameObjectWithTag("Unit").GetComponent<Animator>();
+
+			Debug.Log ("Pose 2 Animation");
+			activePos = 2;
+			anim.SetInteger("Pose", activePos); 
+			//anim.SetInteger("Pose", 0); 
+
+		}
+    }
+
+    public void AddOrRemoveClothes(string naked, string clothesType, string clothesFileName, int wornItemsIndex)
+    {
+		//If the model is naked then we will attach the dress to the model. 
+        if (attachScript.wornItems[wornItemsIndex].FileName == naked)
+        {
+			
+			changeClothesScript.AttachClothingItem(clothesType, clothesFileName);
+        }
+        else if (attachScript.wornItems[wornItemsIndex].FileName == clothesFileName)
+        {
+			changeClothesScript.RemoveClothingItem(clothesType, clothesFileName);
+        }
+    }
+}
---
+++ 3d try on- clothes/Assets/Script/cloth/ItemDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+	public List<ClothingItem> clothesList = new List<ClothingItem>();
+    public static ItemDatabase instance; 
+
+
+    private void Awake()
+    {
+        //create singleton
+		//Debug.Log ("Happen");
+        instance = this; 
+
+		//1.Naked (Empty/No dress/0)
+		clothesList.Add(new ClothingItem(0, "", "", "naked", "Dress"));
+
+		//2.Dresses
+									//ItemID //Name //Description //FileName //ItemType //GameObjectInstance
+
+		clothesList.Add(new ClothingItem(1, "", "", "dress01_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress01_s")));
+		clothesList.Add(new ClothingItem(2, "", "", "dress02_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress02_s")));
+		clothesList.Add(new ClothingItem(3, "", "", "dress03_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress03_s")));
+		clothesList.Add(new ClothingItem(4, "", "", "dress04_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress04_s")));
+		clothesList.Add(new ClothingItem(5, "", "", "dress05_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress05_s")));
+		clothesList.Add(new ClothingItem(6, "", "", "dress06_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress06_s")));
+		clothesList.Add(new ClothingItem(7, "", "", "dress07_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress07_s")));
+		clothesList.Add(new ClothingItem(8, "", "", "dress08_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress08_s")));
+
+		clothesList.Add(new ClothingItem(9, "", "", "dress01_m", "Dress",  (GameObject)Resources.Load("Dresses/medium/dress01_m")));
+		clothesList.Add(new ClothingItem(10, "", "", "dress02_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress02_m")));
+		clothesList.Add(new ClothingItem(11, "", "", "dress03_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress03_m")));
+		clothesList.Add(new ClothingItem(12, "", "", "dress04_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress04_m")));
+		clothesList.Add(new ClothingItem(13, "", "", "dress05_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress05_m")));
+		clothesList.Add(new ClothingItem(14, "", "", "dress06_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress06_m")));
+		clothesList.Add(new ClothingItem(15, "", "", "dress07_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress07_m")));
+		clothesList.Add(new ClothingItem(16, "", "", "dress08_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress08_m")));
+
+		clothesList.Add(new ClothingItem(17, "", "", "dress01_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress01_l")));
+		clothesList.Add(new ClothingItem(18, "", "", "dress02_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress02_l")));
+		clothesList.Add(new ClothingItem(19, "", "", "dress03_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress03_l")));
+		clothesList.Add(new ClothingItem(20, "", "", "dress04_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress04_l")));
+		clothesList.Add(new ClothingItem(21, "", "", "dress05_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress05_l")));
+		clothesList.Add(new ClothingItem(22, "", "", "dress06_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress06_l")));
+		clothesList.Add(new ClothingItem(23, "", "", "dress07_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress07_l")));
+		clothesList.Add(new ClothingItem(24, "", "", "dress08_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress08_l")));
+
+		clothesList.Add(new ClothingItem(25, "", "", "dress09_s", "Dress", (GameObject)Resources.Load("Dresses/small/dress09_s")));
+		clothesList.Add(new ClothingItem(26, "", "", "dress09_m", "Dress", (GameObject)Resources.Load("Dresses/medium/dress09_m")));
+		clothesList.Add(new ClothingItem(27, "", "", "dress09_l", "Dress", (GameObject)Resources.Load("Dresses/large/dress09_l")));
+
+
+
+	}
+
+	public ClothingItem FetchItemByID(int id)
+    {
+		for (int i = 0; i < clothesList.Count; i++)
+        {
+			if (clothesList[i].ItemID == id)
+            {
+				return clothesList[i];
+            }
+        }
+        return null;
+    }
+
+	public ClothingItem FetchItemByFileName(string file)
+    {
+		for (int i = 0; i < clothesList.Count; i++)
+        {
+
+			if (clothesList[i].FileName == file)
+            {
+				return clothesList[i];
+            }
+        }
+        return null;
+    }
+
+
+}
